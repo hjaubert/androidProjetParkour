827df5bfd9e94079004d49fef46fc9b9afb130b9
Julien Moisson <moisson232@unicaen.fr>
2025-04-01 15:14:25 +0200

    vérification du transfert de l'ID de la compétition

--- app/src/main/java/com/example/androidprojetparkour/vue/vueListCompetitionsCompetitors.kt
+++ app/src/main/java/com/example/androidprojetparkour/vue/vueListCompetitionsCompetitors.kt
@@ -1,10 +1,19 @@
 package com.example.androidprojetparkour.vue
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
 
 @Composable
 fun vueListCompetitionsCompetitors(viewModel: ViewModelProvider, competition: Int){
-    Text("Competitors")
+    Column {
+        Spacer(Modifier.height(500.dp))
+        Text("id " + competition)
+    }
+
 }
