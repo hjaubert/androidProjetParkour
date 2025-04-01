87bc8a00e53a91025ef48e245319b3502bd81387
Julien Moisson <moisson232@unicaen.fr>
2025-04-01 18:52:49 +0200

    affichage de la liste des obstacles en fonction du parkour

--- app/src/main/java/com/example/androidprojetparkour/vue/vueListObstacles.kt
+++ app/src/main/java/com/example/androidprojetparkour/vue/vueListObstacles.kt
@@ -3,11 +3,13 @@ package com.example.androidprojetparkour.vue
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -16,7 +18,10 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -24,22 +29,26 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavHostController
 import com.example.androidprojetparkour.api.NetworkResponse
+import com.example.androidprojetparkour.api.models.competitions.Competitions
+import com.example.androidprojetparkour.viewModel.CompetitionViewModel
 import androidx.compose.ui.graphics.Color
-import com.example.androidprojetparkour.api.models.obstacles.Obstacles
+import com.example.androidprojetparkour.api.models.competitions.CompetitionsItem
+import com.example.androidprojetparkour.api.models.obstacles.ObstaclesCourse
+import com.example.androidprojetparkour.router.Routes
 import com.example.androidprojetparkour.viewModel.ObstacleViewModel
 
 
 @Composable
 fun vueListObstales(viewModel: ViewModelProvider, data: Int, navController: NavHostController){
-
     val viewModelObstacle = viewModel[ObstacleViewModel::class.java]
-    val ObstacleResult = viewModelObstacle.obstacles.observeAsState()
+    val obstacleResult = viewModelObstacle.courseObstacles.observeAsState()
 
     LaunchedEffect(Unit) {
-        viewModelObstacle.getObstacleDetails(data)
+        viewModelObstacle.getCourseObstacles(data)
     }
+
     Column {
-        when(val result = ObstacleResult.value){
+        when(val result = obstacleResult.value){
             is NetworkResponse.Error -> {
                 Text(text = result.message)
             }
@@ -47,7 +56,7 @@ fun vueListObstales(viewModel: ViewModelProvider, data: Int, navController: NavH
                 CircularProgressIndicator()
             }
             is NetworkResponse.Success -> {
-                ParkourDetails(data = result.data,navController)
+                listObstacle(data = result.data,navController)
             }
             null -> {}
         }
@@ -56,13 +65,15 @@ fun vueListObstales(viewModel: ViewModelProvider, data: Int, navController: NavH
 }
 
 @Composable
-fun ParkourDetails(data: Obstacles, navController: NavHostController){
+fun listObstacle(data: ObstaclesCourse, navController: NavHostController){
+
     Box(modifier = Modifier.fillMaxSize()) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
 
             ) {
+
             Spacer(modifier = Modifier.height(35.dp))
 
             Text("List of Obstacles", fontSize = 40.sp)
@@ -77,7 +88,7 @@ fun ParkourDetails(data: Obstacles, navController: NavHostController){
                     }, modifier = Modifier.fillMaxWidth().padding(15.dp)) {
                         Column (horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
                             Text(
-                                text = obstacle.name,
+                                text = obstacle.obstacle_name,
                                 fontSize = 30.sp
                             )
 
